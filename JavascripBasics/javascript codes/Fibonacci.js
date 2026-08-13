1049b3553745fed9231015fab90fc524f1fc4135
student <student@16DCEB217B-18.SVV.local>
2026-08-13 16:51:49 +0530

    folder structure update

--- JavascripBasics/javascript codes/Fibonacci.js
+++ JavascripBasics/javascript codes/Fibonacci.js
@@ -1,7 +1,6 @@
-// Import readline.
+
 const readline = require("readline");
 
-// Create readline interface.
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
